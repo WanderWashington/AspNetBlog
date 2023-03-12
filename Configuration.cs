689e2659185385fc8d4cdc6d534162f2e5529a53
WanderWashington <wander.wsantos@gmail.com>
2023-03-12 04:20:47 -0300

    Configuration Improvements Organization of Program Class.

--- Configuration.cs
+++ Configuration.cs
@@ -6,7 +6,17 @@
         public static string JwtKey = "ZmVkYW3ZDg4NjNiNDhlMTk3YjkyOdkNDkyYjcwOGU=";
         public static string ApiKeyName = "api_key";
         public static string ApiKey = "curso_api_IlTevUM/z0ey3NmCV/unWG==";
+        public static SmtpConfiguration Smtp = new SmtpConfiguration();
 
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
 
+            public int Port { get; set; }
+
+            public string UserName { get; set; }
+
+            public string Password { get; set; }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,35 +7,15 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-builder.Services.AddAuthentication(x =>
-{
-    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(x =>
-{
-    x.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
+ConfigureAuthentication(builder);
 
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options =>
-    {
-        options.SuppressModelStateInvalidFilter = true;
-    });//inibe a validação automatica do aspnet do modelo. ModelState.IsValid example.
+ConfigureMvc(builder);
 
-builder.Services.AddDbContext<BlogDataContext>();
-builder.Services.AddTransient<TokenService>();
-//builder.Services.AddTransient(); //sempre criará um novo.
-//builder.Services.AddScoped(); // é por transação/Requisição
-//builder.Services.AddSingleton(); // 1por app, mantem na memoria.
+ConfigureServices(builder);
 
 var app = builder.Build();
+//app.Configuration.GetSection(); //Faz o parse do settings pra uma classe... 
+LoadConfiguration(app);
 
 app.UseAuthentication();
 app.UseAuthorization();
@@ -43,3 +23,52 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+void ConfigureAuthentication(WebApplicationBuilder app)
+{
+    var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+    builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    }).AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
+}
+
+void LoadConfiguration(WebApplication app)
+{
+    Configuration.JwtKey = app.Configuration.GetValue<string>("JwtKey");
+    Configuration.ApiKeyName = app.Configuration.GetValue<string>("ApiKeyName");
+    Configuration.ApiKey = app.Configuration.GetValue<string>("JwtKey");
+
+    var smtp = new Configuration.SmtpConfiguration();
+    app.Configuration.GetSection("Smtp").Bind(smtp);
+    Configuration.Smtp = smtp;
+}
+
+void ConfigureMvc(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });//inibe a validação automatica do aspnet do modelo. ModelState.IsValid example.
+}
+
+void ConfigureServices(WebApplicationBuilder builder)
+{
+    builder.Services.AddDbContext<BlogDataContext>();
+    builder.Services.AddTransient<TokenService>();
+    //builder.Services.AddTransient(); //sempre criará um novo.
+    //builder.Services.AddScoped(); // é por transação/Requisição
+    //builder.Services.AddSingleton(); // 1por app, mantem na memoria.
+}
